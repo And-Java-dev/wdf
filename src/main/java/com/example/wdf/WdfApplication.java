f92d4da67d8922c176dda2487c1ba55b96914b00
And-Java-dev <077161307a>
2020-01-20 00:01:38 +0400

    Initial commit

---
+++ src/main/java/com/example/wdf/WdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.wdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wdf/WdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
